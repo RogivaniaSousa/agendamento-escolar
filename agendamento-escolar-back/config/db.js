1c28179bcc497468c07a791251c75e050a478fd3
RogivaniaSousa <rogivania.sousa@aluno.ce.gov.br>
2026-06-27 19:34:58 -0300

    Versão final do sistema

--- agendamento-escolar-back/config/db.js
+++ agendamento-escolar-back/config/db.js
@@ -1,11 +1,30 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = db;
+(async () => {
+  try {
+    const conn = await pool.getConnection();
+    console.log("✅ Banco conectado com sucesso!");
+
+    const [rows] = await conn.query("SELECT DATABASE() AS banco");
+    console.log(rows);
+
+    conn.release();
+  } catch (err) {
+    console.error("❌ ERRO NA CONEXÃO:");
+    console.error(err);
+  }
+})();
+
+module.exports = pool;
